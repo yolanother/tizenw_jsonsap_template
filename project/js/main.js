0af65c89d48bdfa3fa93ea09fcc575e9e26475a0
Aaron Jackson <a1.jackson@samsung.com>
2014-10-23 15:38:49 -0500

    Updated the template to match JsonSAP library use.

--- project/js/main.js
+++ project/js/main.js
@@ -1,12 +1,49 @@
+var gSap;
+
+var MSG_HELLO = "msg-hello";
+
+function log(message) {
+    $('#textbox').html($('#textbox').html() + message + "<br/>\n");
+}
+
+function onConnected() {
+    // TODO Handle any post connection initializations needed
+    log("Connected to phone!");
+    var data = {
+            'timestamp': Date.now(),
+            'message': "Hello from your Gear!"
+        };
+    gSap.send(MSG_HELLO, data, function(err) {
+        // TODO Process transmission errors.
+    });
+}
+
+function onConnectionFailed(error) {
+    // TODO Handle connection failure
+    log("Connection to phone failed: " + error);
+}
+
+function onDataReceived(type, data) {
+    // TODO Process received data
+    console.log("Received: " + type);
+    console.log(JSON.stringify(data));
+    log("<strong>" + type + "</strong>\n" +
+            "<pre>" + JSON.stringify(data) + "</pre>\n");
+}
 
 $(window).load(function(){
-	document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
-	
-	
-	$('.contents').on("click", function(){
-		$('#textbox').html($('#textbox').html() == "Basic" ? "Sample" : "Basic");				
-	});
+
+
+    $('.contents').on("click", function(){
+        $('#textbox').html("");
+    });
+
+    var providerAppName = "JsonSapApp";
+    var channel = 104;
+    gSap = new JsonSap(providerAppName, channel);
+    gSap.connect(onConnected, onConnectionFailed, onDataReceived);
 });
